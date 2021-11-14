7a2e4173fadefd1dd8d6f5110f5e5ce0596e5dec
katiekachoo <90232915+katiekachoo@users.noreply.github.com>
2021-11-14 13:54:42 -0700

    added alt for each img & relative alt text

--- src/java.js
+++ src/java.js
@@ -75,6 +75,7 @@ function showCityTemp(response) {
   cityTemp.innerHTML = `${temp}`;
   cityDescrip.innerHTML = `${response.data.weather[0].description}`;
   icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  icon.setAttribute("alt", `${response.data.weather[0].description}`)
   country.innerHTML = `${response.data.sys.country}`;
   cityName.innerHTML = `${response.data.name}`;
   lowTempData.innerHTML = `${lowTemp}`;
